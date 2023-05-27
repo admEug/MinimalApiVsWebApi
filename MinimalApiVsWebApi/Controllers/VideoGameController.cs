57715f4b721c8bb168f76738c53d7c7ecccb7418
admEug <zmaeul@gmail.com>
2023-05-27 15:40:36 +0300

    clean code

--- MinimalApiVsWebApi/Controllers/VideoGameController.cs
+++ MinimalApiVsWebApi/Controllers/VideoGameController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MinimalApiVsWebApi.Controllers
 {
-    [Route("api/[controller]")]
+	[Route("api/[controller]")]
     [ApiController]
     public class VideoGameController : ControllerBase
     {
@@ -24,8 +23,12 @@ namespace MinimalApiVsWebApi.Controllers
         public async Task<ActionResult<List<VideoGame>>> GetVideoGame(int id)
         {
             var game = await _context.VideoGames.FindAsync(id);
+
             if (game == null)
-                return NotFound("No game here. :/");
+            {
+				return NotFound("No game here. :/");
+			}
+                
             return Ok(game);
         }
 
@@ -33,7 +36,9 @@ namespace MinimalApiVsWebApi.Controllers
         public async Task<ActionResult<List<VideoGame>>> CreateVideoGame(VideoGame game)
         {
             _context.VideoGames.Add(game);
+
             await _context.SaveChangesAsync();
+
             return Ok(await _context.VideoGames.ToListAsync());
         }
 
@@ -41,14 +46,18 @@ namespace MinimalApiVsWebApi.Controllers
         public async Task<ActionResult<List<VideoGame>>> UpdateVideoGame(VideoGame game, int id)
         {
             var dbGame = await _context.VideoGames.FindAsync(id);
+
             if (dbGame == null)
-                return NotFound("No game here. :/");
+            {
+				return NotFound("No game here. :/");
+			}                
 
             dbGame.Name = game.Name;
             dbGame.Developer = game.Developer;
             dbGame.Release = game.Release;
 
             await _context.SaveChangesAsync();
+
             return Ok(await _context.VideoGames.ToListAsync());
         }
 
@@ -56,8 +65,11 @@ namespace MinimalApiVsWebApi.Controllers
         public async Task<ActionResult<List<VideoGame>>> DeleteVideoGame(int id)
         {
             var dbGame = await _context.VideoGames.FindAsync(id);
+
             if (dbGame == null)
-                return NotFound("No game here. :/");
+            {
+				return NotFound("No game here. :/");
+			}                
 
             _context.VideoGames.Remove(dbGame);
             await _context.SaveChangesAsync();
